1100901662071ae149081322d7bf8522e9c5efda
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-11-05 15:41:52 +0000

    [Add] レビュー編集

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,23 +1,29 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-  end
-
-  def create
-  end
-
   def show
-    @user = current_user
     @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to songs_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:song_id, :listen, :text)
   end
 end
