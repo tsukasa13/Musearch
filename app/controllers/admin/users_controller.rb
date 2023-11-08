eac0a091b3027224e0f1534ffc1c92c012cd47af
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-11-08 16:10:30 +0000

    [Add] ユーザー一覧

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,16 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
