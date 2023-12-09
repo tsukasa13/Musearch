5c3a6a90a5dca242bc52ff5b005edc0d4e5fc665
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2023-12-09 16:06:52 +0000

    [Fix] ログイン後、ログアウト後等のリンク先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
-      admin_root_path
+      admin_songs_path
     else
-      root_path
+      songs_path
     end
   end
 
